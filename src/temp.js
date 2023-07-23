76594f31f651aeb0110c4ff077aa27a6d48d0951
sarmistha2001 <satrusallyasarmistha@gmail.com>
2023-07-23 12:37:50 -0500

    update build for gh-pages

--- src/temp.js
+++ src/temp.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import WeatherIcon from 'react-icons-weather';
 import "./index.css"
 
 
@@ -21,21 +22,21 @@ export default function Temp(){
             <div className="box">
 
                 <div className="inputData">
+                <h1 style={{color:"red"}}>ENTER  THE LOCATION</h1>
 <input type="search" value={search} className="inputField"  onChange={(e)=>{setsearch(e.target.value)}}/>
                 </div>
-{!city?(<p>no data found</p>):(<>
+{!city?(<p style={{color:"red"}}>no data found</p>):(<>
     <div className="info">
+    <div className="weather">⛅️</div>
                  <h1 className="location">
+                 
                  <i className="fas fa-street-view"></i>{search}
                  </h1>
-                 <h2 className="temp">{city.temp}°Cel</h2>
+                
+                 <h2 className="temp"><span style={{color:"red"}}>Temp:</span>:{city.temp}°Cel</h2>
                  <h3 className="tempmin_max">Max:{city.temp_max}°Cel   Min:{city.temp_min}°Cel</h3>
             </div>
-            <div className="ocean">
-            <div className="wave"></div>
-            <div className="wave"></div>
-            <div className="wave"></div>
-            </div>
+            
 </>)}
 
 
